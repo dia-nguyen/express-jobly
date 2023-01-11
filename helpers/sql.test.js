d3d94f676d483199bee39fc92f23f207d81b92b4
Christopher Maltez <cflux.maltez@live.com>
2023-01-11 11:35:26 -0800

    completed part 1

--- helpers/sql.test.js
+++ helpers/sql.test.js
@@ -6,15 +6,34 @@ const {
   UnauthorizedError,
 } = require("../expressError");
 
-const { sqlForPartialUpdate } = require("../helpers/sql")
-
-describe("Sql Partial Update", function(){
-  test("bad request if no data", function() {
+const { sqlForPartialUpdate } = require("../helpers/sql");
+// Part 1
+describe("Sql Partial Update", function () {
+  test("bad request if no data", function () {
     try {
-      sqlForPartialUpdate({}, {});
+      sqlForPartialUpdate({},{});
       // throw new Error("fail test, you shouldn't get here");
     } catch (err) {
       expect(err instanceof BadRequestError).toBeTruthy();
     }
-  })
-})
+  });
+
+  test("testing for valid data", function () {
+    const updateData = {
+      firstName: "NewF",
+      lastName: "NewF",
+      email: "new@email.com",
+      isAdmin: true,
+    };
+    const jsToUpdate = {
+      firstName: "first_name",
+      lastName: "last_name",
+      isAdmin: "is_admin",
+    };
+    const updatedData = sqlForPartialUpdate(updateData, jsToUpdate);
+    expect(updatedData).toEqual({
+      setCols: '"first_name"=$1, "last_name"=$2, "email"=$3, "is_admin"=$4',
+      values: ["NewF", "NewF", "new@email.com", true],
+    });
+  });
+});
