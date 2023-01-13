868c831d737dd5a031014195ac3b4402061d7f4e
Christopher Maltez <cflux.maltez@live.com>
2023-01-12 17:09:01 -0800

    git stash popped.

--- models/job.test-for-later.js
+++ models/job.test-for-later.js
@@ -1,42 +1,46 @@
-// "use strict";
+"use strict";
 
-// const db = require("../db.js");
-// const { BadRequestError, NotFoundError } = require("../expressError");
-// const Job = require("./job.js");
-// const {
-//   commonBeforeAll,
-//   commonBeforeEach,
-//   commonAfterEach,
-//   commonAfterAll,
-// } = require("./_testCommon");
+const db = require("../db.js");
+const { BadRequestError, NotFoundError } = require("../expressError");
+const Job = require("./job.js");
+const {
+  commonBeforeAll,
+  commonBeforeEach,
+  commonAfterEach,
+  commonAfterAll,
+} = require("./_testCommon");
 
-// beforeAll(commonBeforeAll);
-// beforeEach(commonBeforeEach);
-// afterEach(commonAfterEach);
-// afterAll(commonAfterAll);
+beforeAll(commonBeforeAll);
+beforeEach(commonBeforeEach);
+afterEach(commonAfterEach);
+afterAll(commonAfterAll);
 
-// /************************************** create */
+/************************************** create */
 
-// describe("create a job", function () {
-//   const newJob = {
-//     title: "testJob",
-//     companyHandle: "c2",
-//     salary: "50000",
-//     equity: 0
-//   }
+describe("create a job", function () {
+  const newJob = {
+    title: "testJob",
+    companyHandle: "c2",
+    salary: "50000",
+    equity: 0
+  }
 
-//   test("works", async function() {
-//     let job = await Job.create(newJob);
-//     expect(job).toEqual(newJob);
+  test("works", async function() {
+    let job = await Job.create(newJob);
+    expect(job).toEqual(newJob);
 
-//     const results = await db.query(
-//       `SELECT title, company_handle, salary, equity
-//         FROM jobs
-//         WHERE title = 'testJob'`
-//     );
-//     expect(result.rows).toEqual([
-//       newJob
-//     ])
-//   });
-// })
+    const results = await db.query(
+      `SELECT title, company_handle, salary, equity
+        FROM jobs
+        WHERE title = 'testJob'`
+    );
+    expect(result.rows).toEqual([
+      newJob
+    ])
+  });
+})
 
+/************************************** FindAll */
+describe("findAll", function () {
+  test("")
+})
